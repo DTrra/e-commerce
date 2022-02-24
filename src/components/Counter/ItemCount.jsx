d73bb9134c7eab32a585323e3a97377807bf1eea
Diego Terranova <daterranova@gmail.com>
2022-02-24 16:52:17 -0300

    arreglo en Counter

--- src/components/Counter/ItemCount.jsx
+++ src/components/Counter/ItemCount.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from 'react';
 import './ItemCount.css';
 
-const Counter = ({ stock, initial, addToCart }) => {
+const Counter = ({ stock, onAdd }) => {
     const [number, setNumber] = useState(0);
-    const add = () => { 
+
+    const add = () => {
         number < stock && setNumber(number + 1);
     };
+
     const substract = () => {
-        number > initial && setNumber(number - 1);
+        number > 0 && setNumber(number - 1);
     };
 
     return (
-        <div className="container">
-            <div className="container-buton">
-                <div className="container-add-substract">
+        <div className="container-buton">
+            <div className="container-add-substract">
                 <button onClick={substract}>-</button>
                 <p>{number}</p>
                 <button onClick={add}>+</button>
-                </div>
-                <div>
-                    <button 
-                        disabled={number === 0}
-                        className={number === 0 ? 'disabled' : 'add'}
-                        onClick={() => addToCart(number)}>
-                        Enviar al Repartidor
-                    </button>
-                </div>
+            </div>
+            <div>
+                <button
+                    disabled={number === 0}
+                    className={number === 0 ? 'disabled' : 'add'}
+                    onClick={() => onAdd(number)}
+                >
+                    Enviar al Repartidor
+                </button>
             </div>
         </div>
-
     );
-}
+};
 export default Counter;
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,32 +1,37 @@
+import React, { useState } from 'react';
 import Counter from '../Counter/ItemCount';
 import Card from 'react-bootstrap/Card'
 
 const ItemDetail = ({ product }) => {
 
-    return (
-<div className="container">
-    <div className='row justify-content-center'>
-    <Card border="danger shadow " style={{ width: '28rem', marginTop: '30px', marginButton: '30px' }}>
-    <Card.Header className='shadow text-center'><img src={product?.img} width="400px" alt='imagen'/></Card.Header>
-    <Card.Body>
-        <Card.Title>
-            {product?.name}
-        </Card.Title>
-            <Card.Text>
-                Categoria: {product?.category}
-            </Card.Text>
-            <Card.Text >
-                Descripcion: {product?.description}
-            </Card.Text>
-            <Card.Title >
-                Precio: ${product?.price}
-            </Card.Title>
-        <Counter className='justify-content-center' />
-    </Card.Body>
-    </Card>
-    </div>
-</div> 
+    const [qty, setQty] = useState(0);
+    const onAdd = (cantidad) => {
+        setQty(cantidad);
+    };
 
+    return (
+    <div className="container">
+        <div className='row justify-content-center'>
+        <Card border="danger shadow " style={{ width: '28rem', marginTop: '30px', marginButton: '30px' }}>
+        <Card.Header className='shadow text-center'><img src={product?.img} width="400px" alt='imagen'/></Card.Header>
+        <Card.Body>
+            <Card.Title>
+                {product?.name}
+            </Card.Title>
+                <Card.Text>
+                    Categoria: {product?.category}
+                </Card.Text>
+                <Card.Text >
+                    Descripcion: {product?.description}
+                </Card.Text>
+                <Card.Title >
+                    Precio: ${product?.price}
+                </Card.Title>
+            <Counter stock={product?.stock} onAdd={onAdd} className='justify-content-center' />
+        </Card.Body>
+        </Card>
+        </div>
+    </div> 
     )
 }
 export default ItemDetail;
